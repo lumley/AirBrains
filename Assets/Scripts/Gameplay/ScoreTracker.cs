cd66a78de53003edce19cc83555d08f5cda38dc1
Aaron <aaron.chapin@gmail.com>
2017-10-08 18:14:06 +0200

    Points are tracked, and the game can be finished!

--- Assets/Scripts/Gameplay/ScoreTracker.cs
+++ Assets/Scripts/Gameplay/ScoreTracker.cs
@@ -3,4 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreTracker : MonoBehaviour {
+
+	protected int currentScore;
+
+	public int Score {
+		get { return currentScore; }
+	}
+
+	void Start() {
+		currentScore = 0;
+	}
+
+	public int ChangeScore(int delta) {
+		currentScore += delta;
+		return currentScore;
+	}
+
 }
