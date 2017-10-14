879a3ab4e0e23e78518e73ad860054bfdd654edd
Aaron <aaron.chapin@gmail.com>
2017-10-14 18:13:40 +0200

    Added Spawning prefabs - though the Player spawner doesn't have a prefab yet.

---
+++ Assets/Scripts/Gameplay/PrefabReplacer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabReplacer : MonoBehaviour {
+
+	public GameObject prefabToSpawn;
+	public bool replaceOnAwake = false;
+
+	void Awake() {
+		if (replaceOnAwake) {
+			SpawnPrefab ();
+		}
+	}
+
+	public void SpawnPrefab() {
+		GameObject newObject = Instantiate (prefabToSpawn);
+		newObject.transform.SetParent (transform.parent);
+		newObject.transform.localPosition = transform.localPosition;
+		Destroy (gameObject);
+	}
+}
