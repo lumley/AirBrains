838baa8137f339d6e8c6b883fdaed5adc67c9b5f
Sergio R. Lumley <lumley256@gmail.com>
2017-10-29 20:16:34 +0100

    End game now expects any player to press Start game

---
+++ Assets/Scripts/ScreenLogic/Messages/BackToLobbyMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ScreenLogic.Messages
+{
+    [Serializable]
+    public class BackToLobbyMessage
+    {
+        public const string MessageType = "BackToLobby";
+        public const string MessageTypeInvariant = "backtolobby";
+
+        [JsonProperty("type")] public string Type = MessageType;
+    }
+}
