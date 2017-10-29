c1e6c0ac7464856d5bd515be80013820cc3e8ce8
Sergio R. Lumley <lumley256@gmail.com>
2017-10-29 19:12:43 +0100

    Made countdown clock less programmer-artsy

--- Assets/Scripts/Gameplay/Effects/MoveClock.cs
+++ Assets/Scripts/Gameplay/Effects/MoveClock.cs
@@ -1,47 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MoveClock : MonoBehaviour {
+public class MoveClock : MonoBehaviour
+{
+    public Image fillImage;
+    public Text text;
+    public float[] textTriggers;
+    public Color textColor;
+    public Text ChooseYourActionsNowText;
+    public Image ChoseYourActionsBgImage;
 
-	public Image fillImage;
-	public Text text;
-	public float[] textTriggers;
-	public Color textColor;
+    private float lastValue;
+    private GameRunner runner;
+    private Color clearColor;
 
-	private float lastValue;
-	private GameRunner runner;
-	private Color clearColor;
+    private void Awake()
+    {
+        runner = GameRunner.FindInScene();
+        clearColor = new Color(textColor.r, textColor.g, textColor.b, 0f);
+    }
 
-	void Awake() { 
-		runner = GameRunner.FindInScene ();
-		clearColor = new Color (textColor.r, textColor.g, textColor.b, 0f);
-	}
-
-	void Update () {
-		if (runner.TimeLeft > 0f) {
-			if (!fillImage.enabled) {
-				fillImage.enabled = true;
-				text.enabled = true;
-			}
-			fillImage.fillAmount = runner.TimeLeft / runner.secondsToWaitForInput;
-			if (lastValue != -1f) {
-				foreach (float threshhold in textTriggers) {
-					if (lastValue >= threshhold && runner.TimeLeft <= threshhold) {
-						text.text = "" + threshhold;
-						text.color = textColor;
-					}
-				}
-			}
-			text.color = Color.Lerp (text.color, clearColor, .1f);
-			lastValue = runner.TimeLeft;
-		} else {
-			if (fillImage.enabled) {
-				fillImage.enabled = false;
-				text.enabled = false;
-				lastValue = -1f;
-			}
-		}
-	}
+    private void Update()
+    {
+        if (runner.TimeLeft > 0f)
+        {
+            if (!fillImage.enabled)
+            {
+                fillImage.enabled = true;
+                text.enabled = true;
+                ChooseYourActionsNowText.enabled = true;
+                ChoseYourActionsBgImage.enabled = true;
+            }
+            fillImage.fillAmount = runner.TimeLeft / runner.secondsToWaitForInput;
+            if (Math.Abs(lastValue - -1f) > Mathf.Epsilon)
+            {
+                foreach (var threshhold in textTriggers)
+                {
+                    if (lastValue >= threshhold && runner.TimeLeft <= threshhold)
+                    {
+                        text.text = Convert.ToString(threshhold, CultureInfo.InvariantCulture);
+                        text.color = textColor;
+                    }
+                }
+            }
+            text.color = Color.Lerp(text.color, clearColor, .01f);
+            lastValue = runner.TimeLeft;
+        }
+        else if (fillImage.enabled)
+        {
+            fillImage.enabled = false;
+            text.enabled = false;
+            lastValue = -1f;
+            ChooseYourActionsNowText.enabled = false;
+            ChoseYourActionsBgImage.enabled = false;
+        }
+    }
 }
