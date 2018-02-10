c528b401fd15e9dbf2426a46b4d3f2af7a1c0e48
Aaron <aaron.chapin@gmail.com>
2018-02-10 14:45:54 +0100

    Loading happens asynch now, instructions are in the loading screen.

--- Assets/Scripts/Gameplay/GameStarter.cs
+++ Assets/Scripts/Gameplay/GameStarter.cs
@@ -9,17 +9,15 @@ public class GameStarter : MonoBehaviour
     void Start()
     {
         string newScene = availableLevelScenes[Mathf.RoundToInt(Random.value * (availableLevelScenes.Length - 1))];
+        Application.backgroundLoadingPriority = ThreadPriority.Low;
         Debug.Log("Loading " + newScene);
         StartCoroutine(LoadNewScene(newScene));
     }
 
     private IEnumerator LoadNewScene(string newScene) {
-        var result = SceneManager.LoadSceneAsync(newScene);
-        while (!result.isDone) 
-        {
-            Debug.Log ( "progress: " + result.progress );
-            yield return new WaitForEndOfFrame ();
-        }
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadSceneAsync(newScene);
+        yield return new WaitForSeconds(.5f);
     }
 
 }
