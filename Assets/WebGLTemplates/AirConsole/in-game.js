be35e399ee26c6eabcc1f36517e954966a668e18
Sergio R. Lumley <lumley256@gmail.com>
2018-02-10 18:25:22 +0100

    Added character head while playing game, hiding actions while game is playing

--- Assets/WebGLTemplates/AirConsole/in-game.js
+++ Assets/WebGLTemplates/AirConsole/in-game.js
@@ -3,19 +3,22 @@ var slotClassName = "slot";
 var slotButtonClassName = "slot-button";
 var activeSlotClassName = "slot-active";
 var optionClassName = "option";
+var optionsClassName = "options";
 var activeOptionClassName = "option-active";
 var readyClassName = "ready";
 var readyButtonClassName = "ready-button";
 var buttonDisabled = "button-disabled";
 var readyButtonActiveClassName = "ready-button--active";
+var inGameAvatarClassName = "in-game-avatar";
+var hiddenClass = "hidden";
 
 var optionTexts = [
-  "<img src=\"images/icon_up.png\" width=\"28\" height=\"28\"/>",
-  "<img src=\"images/icon_left.png\" width=\"28\" height=\"28\"/>",
-  "<img src=\"images/icon_stop.png\" width=\"28\" height=\"28\"/>",
-  "<img src=\"images/icon_right.png\" width=\"28\" height=\"28\"/>",
-  "<img src=\"images/icon_down.png\" width=\"28\" height=\"28\"/>",
-  "<img src=\"images/icon_random_lightblue.png\" width=\"28\" height=\"28\"/>"
+    "<img src=\"images/icon_up.png\" width=\"28\" height=\"28\"/>",
+    "<img src=\"images/icon_left.png\" width=\"28\" height=\"28\"/>",
+    "<img src=\"images/icon_stop.png\" width=\"28\" height=\"28\"/>",
+    "<img src=\"images/icon_right.png\" width=\"28\" height=\"28\"/>",
+    "<img src=\"images/icon_down.png\" width=\"28\" height=\"28\"/>",
+    "<img src=\"images/icon_random_lightblue.png\" width=\"28\" height=\"28\"/>"
 ];
 
 var optionIds = ["up", "left", "wait", "right", "down"];
@@ -36,139 +39,149 @@ var slotElements = document.getElementsByClassName(slotClassName);
 var optionElements = document.getElementsByClassName(optionClassName);
 var readyElement = document.getElementsByClassName(readyClassName)[0];
 var readyButtonElement = readyElement.getElementsByClassName(readyButtonClassName)[0];
+var inGameAvatarElement = document.getElementsByClassName(inGameAvatarClassName)[0];
+var optionsElement = document.getElementsByClassName(optionsClassName)[0];
 
-[].slice.call(slotElements).forEach(function(slot, index) {
-  slot.addEventListener("touchstart", function(){
-    selectSlot(index);
-  });
+[].slice.call(slotElements).forEach(function (slot, index) {
+    slot.addEventListener("touchstart", function () {
+        selectSlot(index);
+    });
 });
 
-[].slice.call(optionElements).forEach(function(option, index) {
-  option.addEventListener("touchstart", function(){
-      selectOption(index, true);
-  });
+[].slice.call(optionElements).forEach(function (option, index) {
+    option.addEventListener("touchstart", function () {
+        selectOption(index, true);
+    });
 });
 
-readyElement.addEventListener("touchstart", function(){
-  activateReady();
+readyElement.addEventListener("touchstart", function () {
+    activateReady();
 });
 
 selectSlot(0);
 
 function selectSlot(slotId) {
-  if (selectedSlotId != undefined) {
-    unselectSlot();
-  }
+    if (selectedSlotId !== undefined) {
+        unselectSlot();
+    }
 
-  slotElements[slotId].classList.add(activeSlotClassName);
+    slotElements[slotId].classList.add(activeSlotClassName);
 
-  selectedSlotId = slotId;
+    selectedSlotId = slotId;
 
-  selectOption(selectedOptions[slotId]);
+    selectOption(selectedOptions[slotId]);
 }
 
 function unselectSlot() {
-  unselectOption(selectedOptions[selectedSlotId]);
-  var slot = document.getElementsByClassName(activeSlotClassName)[0];
-  slot.classList.remove(activeSlotClassName);
-  selectedSlotId = undefined;
+    unselectOption(selectedOptions[selectedSlotId]);
+    var slot = document.getElementsByClassName(activeSlotClassName)[0];
+    slot.classList.remove(activeSlotClassName);
+    selectedSlotId = undefined;
 }
 
 function selectOption(optionId, autoSelectNextSlot) {
-  if (selectedSlotId != undefined) {
-    unselectOption(selectedOptions[selectedSlotId]);
-  }
-
-  if (optionId == optionUndefined) {
-    return;
-  }
-
-  optionElements[optionId].classList.add(activeOptionClassName);
-
-  slotElements[selectedSlotId].getElementsByClassName(slotButtonClassName)[0].innerHTML = optionTexts[optionId];
-
-  selectedOptions[selectedSlotId] = optionId;
-
-  onActionSelected(optionId, selectedSlotId);
-
-  var newAllSlotSelected = (selectedOptions.filter(function(option) {return option === optionUndefined;}).length === 0);
-
-  if (autoSelectNextSlot && !allSlotSelected)
-  {
-    if (selectedSlotId < slotElements.length - 1) {
-      selectSlot(selectedSlotId + 1);
+    if (selectedSlotId != undefined) {
+        unselectOption(selectedOptions[selectedSlotId]);
     }
-  }
 
-  if (allSlotSelected === false && newAllSlotSelected) {
-    enableReady();
-  }
+    if (optionId == optionUndefined) {
+        return;
+    }
 
-  allSlotSelected = newAllSlotSelected;
+    optionElements[optionId].classList.add(activeOptionClassName);
+
+    slotElements[selectedSlotId].getElementsByClassName(slotButtonClassName)[0].innerHTML = optionTexts[optionId];
+
+    selectedOptions[selectedSlotId] = optionId;
+
+    onActionSelected(optionId, selectedSlotId);
+
+    var newAllSlotSelected = (selectedOptions.filter(function (option) {
+        return option === optionUndefined;
+    }).length === 0);
+
+    if (autoSelectNextSlot && !allSlotSelected) {
+        if (selectedSlotId < slotElements.length - 1) {
+            selectSlot(selectedSlotId + 1);
+        }
+    }
+
+    if (allSlotSelected === false && newAllSlotSelected) {
+        enableReady();
+    }
+
+    allSlotSelected = newAllSlotSelected;
 }
 
 function unselectOption(optionId) {
-  if (optionId != optionUndefined) {
-    optionElements[optionId].classList.remove(activeOptionClassName);
-  }
+    if (optionId != optionUndefined) {
+        optionElements[optionId].classList.remove(activeOptionClassName);
+    }
 }
 
 function activateReady() {
-  // ready = !ready;
-  //
-  // if (ready) {
-  //   readyButtonElement.classList.add(readyButtonActiveClassName);
-  //   onPlayerReady();
-  // } else {
-  //   readyButtonElement.classList.remove(readyButtonActiveClassName);
-  //   onPlayerNotReady();
-  // }
+    // ready = !ready;
+    //
+    // if (ready) {
+    //   readyButtonElement.classList.add(readyButtonActiveClassName);
+    //   onPlayerReady();
+    // } else {
+    //   readyButtonElement.classList.remove(readyButtonActiveClassName);
+    //   onPlayerNotReady();
+    // }
 }
 
 function enableReady() {
-  ready = true;
-  onPlayerReady();
-  readyButtonElement.disabled = false;
+    ready = true;
+    onPlayerReady();
+    readyButtonElement.disabled = false;
 
-  readyButtonElement.classList.remove(buttonDisabled);
+    readyButtonElement.classList.remove(buttonDisabled);
 }
 
 function onPlayerNotReady() {
-  console.log("Player is not ready");
+    console.log("Player is not ready");
 }
 
 // ****************************************************************
 // These are relevant functions for communicating with the game :
 
 function onActionSelected(optionId, slotId) {
-  // optionId :
-  // 0 = up;
-  // 1 = right;
-  // 2 = down;
-  // 3 = left;
-  // 4 = stop;
-  // slotId :
-  // from 0 to 3
-  app.setDefinedAction(slotId + 1, optionIds[optionId]);
-  console.log("Action " + optionIds[optionId] + " selected for action slot " + slotId);
+    // optionId :
+    // 0 = up;
+    // 1 = right;
+    // 2 = down;
+    // 3 = left;
+    // 4 = stop;
+    // slotId :
+    // from 0 to 3
+    app.setDefinedAction(slotId + 1, optionIds[optionId]);
+    console.log("Action " + optionIds[optionId] + " selected for action slot " + slotId);
 }
 
 function onPlayerReady() {
-  console.log("Player is ready");
-  app.sendDeviceIsReady(true);
+    console.log("Player is ready");
+    app.sendDeviceIsReady(true);
 }
 
 function resetInGame() {
-  selectSlot(0);
-  selectedOptions = [optionUndefined, optionUndefined, optionUndefined, optionUndefined];
-  ready = false;
-  allSlotSelected = false;
-  slotElements[0].getElementsByClassName(slotButtonClassName)[0].innerHTML = optionTexts[5];
-  slotElements[1].getElementsByClassName(slotButtonClassName)[0].innerHTML = optionTexts[5];
-  slotElements[2].getElementsByClassName(slotButtonClassName)[0].innerHTML = optionTexts[5];
-  slotElements[3].getElementsByClassName(slotButtonClassName)[0].innerHTML = optionTexts[5];
+    optionsElement.classList.remove(hiddenClass);
+    selectSlot(0);
+    selectedOptions = [optionUndefined, optionUndefined, optionUndefined, optionUndefined];
+    ready = false;
+    allSlotSelected = false;
+    slotElements[0].getElementsByClassName(slotButtonClassName)[0].innerHTML = optionTexts[5];
+    slotElements[1].getElementsByClassName(slotButtonClassName)[0].innerHTML = optionTexts[5];
+    slotElements[2].getElementsByClassName(slotButtonClassName)[0].innerHTML = optionTexts[5];
+    slotElements[3].getElementsByClassName(slotButtonClassName)[0].innerHTML = optionTexts[5];
+
+    inGameAvatarElement.classList.add("character-preview-" + currentPlayerSelectedCharacterId);
+}
+
+function blockActionSelection() {
+    optionsElement.classList.add(hiddenClass);
 }
 
 function test() {
-  window.location.href = "";
+    window.location.href = "";
 }
