28db4be2ef0a2cb5a56d7afdb36705d9f8558931
Aaron <aaron.chapin@gmail.com>
2018-02-10 13:16:43 +0100

    Added new loading screen - not finished yet.

--- Assets/Scripts/Gameplay/GameStarter.cs
+++ Assets/Scripts/Gameplay/GameStarter.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameStarter : MonoBehaviour
 {
@@ -9,6 +10,16 @@ public class GameStarter : MonoBehaviour
     {
         string newScene = availableLevelScenes[Mathf.RoundToInt(Random.value * (availableLevelScenes.Length - 1))];
         Debug.Log("Loading " + newScene);
-        SceneManager.LoadScene(newScene);
+        StartCoroutine(LoadNewScene(newScene));
     }
+
+    private IEnumerator LoadNewScene(string newScene) {
+        var result = SceneManager.LoadSceneAsync(newScene);
+        while (!result.isDone) 
+        {
+            Debug.Log ( "progress: " + result.progress );
+            yield return new WaitForEndOfFrame ();
+        }
+    }
+
 }
---
+++ Assets/Scripts/Utilities/LoadingShifter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingShifter : MonoBehaviour {
+
+	public float displayTime = 2f;
+
+	public Transform[] marquees;
+
+	void Start() {
+		StartCoroutine(SetActive(0));
+	}
+
+	private IEnumerator SetActive(int index) {
+		int minMax = 0;
+		foreach(Transform parent in marquees) {
+			for(int childIndex = 0; childIndex < parent.childCount; childIndex++) {
+				parent.GetChild(childIndex).gameObject.SetActive(childIndex == index);
+				if(childIndex > minMax) {
+					minMax = childIndex;
+				}
+			}
+		}
+		Debug.Log("Started Waiting");
+		yield return new WaitForSeconds(displayTime);
+		Debug.Log("Done Waiting");
+		int nextIndex = 0;
+		if(index < minMax) {
+			nextIndex = index + 1;
+		}
+		Debug.Log("Next set! " + nextIndex );
+		StartCoroutine(SetActive(nextIndex));
+	}
+}
