ad67d8165e81ab4ea15e1539f718a45ea6ae31db
Aaron <aaron.chapin@gmail.com>
2018-02-10 14:53:31 +0100

    On less wide screens, the camera zooms out to make more room.

---
+++ Assets/Scripts/Utilities/CameraSizeFix.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSizeFix : MonoBehaviour {
+
+	public float ratio = 1.6f;
+	public float newOrthoSize = 8.2f;
+
+	void Start () {
+		if(((float)Screen.width / (float)Screen.height) < ratio){
+			if(gameObject.GetComponent<Camera>() == null) return;
+			gameObject.GetComponent<Camera>().orthographicSize = newOrthoSize;
+		}
+	}
+}
